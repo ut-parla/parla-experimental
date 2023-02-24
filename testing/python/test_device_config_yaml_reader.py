34c3b87283820004f8660ed9264a4e19c6681f1b
dialecticDolt <will.ruys@gmail.com>
2023-02-23 22:30:53 -0600

    Cleanup debugging prints

--- testing/python/test_device_config_yaml_reader.py
+++ testing/python/test_device_config_yaml_reader.py
@@ -14,11 +14,15 @@ def test_read_yaml():
         cpu_mem_sz = config["CPU"]["mem_sz"]
         gpu_num_devices = config["GPU"]["num_devices"]
         gpu_mem_szs = config["GPU"]["mem_sz"]
+
         assert len(gpu_mem_szs) == gpu_num_devices
+
         assert cpu_num_cores == 48
         assert cpu_mem_sz == 180000000
+
         for mem_sz in gpu_mem_szs:
             assert mem_sz == 11000000
+
         print(f"CPU # of Cores: {cpu_num_cores}, memory size: {cpu_mem_sz},")
         print(
             f"GPU # of Devices: {gpu_num_devices}, memory sizes: {gpu_mem_szs}")
