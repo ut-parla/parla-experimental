70704441c5c5fc99b922d88c53475b320314316c
Hochan Lee <hochan@utexas.edu>
2023-03-17 01:21:13 -0500

    Sort of pseudo code ..

--- benchmark/python/quicksort_mdevice.py
+++ benchmark/python/quicksort_mdevice.py
@@ -25,26 +25,70 @@ def xp_block_partition(start, end, dev_id, num_gpus):
             block_end += (dev_id + 1)
     return block_beg, block_end
 
+"""
+def partition():
+    pass
+"""
+
 def main(T):
     device_id = 0
-    for i in range(4):
-        beg, end = xp_block_partition(0, 102, i, 4)
-        print(i, "-> ", beg, "~", end)
+    num_gpus = 4
+    num_arrays = 1
+    @spawn(T, placement=[(cuda, cuda, cuda, cuda)])
+    def quicksort():
+        while True:
+            remaining_work = False
+            target_lists = []
+            if num_arrays == 1: # which means that the first level.
+                target_lists.append(input_array)
+            for i in range(num_arrays)
+                target = target_lists[i]
+                beg = 0
+                end = len(target)
+                # pivot = find_pivot(beg, end)
+                readidx = []
+                readsize = []
+                for d in range(num_gpus):
+                    d_beg, d_end = xp_block_partition(beg, end, d, num_gpus)
+                    readidx.append(d_beg)
+                    readsize.append(d_end - d_beg + 1)
+                    if (d == num_gpus - 1):
+                        readidx.append(d_end)
+                # x = xp.array(range(end), readidx, readsize)
+                for d in range(num_gpus):
+                    """
+                    with locals.Device[d]:
+                          # Iterates the ranges, and stores items smaller
+                          # than a pivot to low_arr and items bigger than
+                          # a pivot to high_arr
+                          partition(d_beg, d_end, pivot, low_arr[d], high_arr[d])
+                          # TODO(hc): How can we create an array of a vector?
+                          #           We don't know size of low_arr[d] and high_arr[d].
+                    """
+                    pass
+                for d in range(num_gpus):
+                    """
+                    with locals.Device[d]:
+                          # Aggregate low_arr and high_arr from the whole
+                          # devices and merge them to a single array.
+                          # For each level computation (e.g., ith level),
+                          # i^2 arrays are created.
+                          # For example, after the first level computation,
+                          # left and right arrays are created and each of them
+                          # should be sorted separately. 
 
-    """
-    @spawn(T[0])
-    def quick_sort():
-        print("Hello World")
+                          # Let's assume that this loop's outputs are L and R
 
-        # Choose pivot
-        # 
-        # with cp.cuda.Device(device_id):
-        #   partition()
-        # device_id += 1
-        # 
-        # quick_sort()
-        # quick_sort()
-    """
+                          # if L or R is not sorted:
+                          #    remaining_work = True
+                    """
+                    pass
+                if remaining_work == False:
+                    break
+                target_lists.append(L)
+                target_lists.append(R)
+            num_arrays *= 2
+
 
 if __name__ == "__main__":
     with Parla(dev_config_file=args.dev_config):
