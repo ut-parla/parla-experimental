ce90ac7207da31d42ca69be0c90c7d73e0688eac
Hochan Lee <hochan@utexas.edu>
2023-03-27 16:14:54 -0500

    Initialize managed parray tables in parray tracker

--- src/python/parla/common/parray/tracker.py
+++ src/python/parla/common/parray/tracker.py
@@ -6,22 +6,25 @@ from typing import Dict
 PArray = core.PArray
 
 class PArrayTracker:
+  _managed_parray_tbl: Dict[int, Dict[int, bool]]
 
   def __init__(self, device_manager: PyDeviceManager):
       print("PArray tracker is created.")
-      # First key: a PArray ID (NOTE that this is the ID of the complete array)
-      # Second key (a value of the first key): a device ID
+      # First key: a device global ID
+      # Second key (a value of the first key): a PArray ID
+      #            (NOTE that this is the ID of the complete array)
       # Second value: true if the PArray instance exists on the device
-      self._managed_parray_tbl = Dict[int, Dict[int, bool]]
+      # Using a device ID as the first key is time efficient since
+      # we don't need to create device keys whenever we add a new PArray
+      # to the table.
+      self._managed_parray_tbl = {}
       self._device_manager = device_manager
       self._devices = device_manager.get_all_devices()
-      """
-      for d in self._devices:
-          print(d, " - ", d.get_global_id())
-      """
-
       # TODO(hc): add a resource pool or a device manager to update
-      #           memory usage.
+
+  def initialize_table(self):
+      for d in self._devices:
+          self._managed_parray_tbl[d.get_global_id()] = {}
 
   def track_parray(self, parray: PArray, dev_id: int):
       """
@@ -32,7 +35,6 @@ class PArrayTracker:
       print("PArray ID: ", parray.ID, " and its parent ID:",
             parray.parent_ID if parray.ID != parray.parent_ID else None,
             " is tracked.")
-
 
   def untracak_parray(self, parray: PArray, dev_id: int):
       """
