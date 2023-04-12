4df33d496024b3c07ba2659c0fd1a642f2fb1dd9
Hochan Lee <nicelhc13@users.noreply.github.com>
2023-04-11 23:08:19 -0500

    Improve/synthetic workloads script (#75)
    This commit refines and improves the existing test scripts for synthetic workloads. This commit aims to provide a systemic way for synthetic workload benchmarking.

--- benchmark/python/start_benchmark.sh
+++ benchmark/python/start_benchmark.sh
@@ -1,28 +1,109 @@
-#!/bin/bash
+GRAPH_TYPES_STR=( "serial" "independent" "reduction" )
+#GRAPH_TYPES_STR=( "independent" )
+#GRAPH_TYPES_STR=( "serial" )
+#GRAPH_TYPES_STR=( "independent" "reduction" )
+#NUM_TASKS_SET=( 300 500 1000 2000 )
+NUM_TASKS_SET=( 1000 )
+#NUM_TASKS_SET=( 10 )
+#LEVELS=( 8 16 )
+LEVELS=( 8 )
+#SLEEP_KNOBS=( 3000 5000 10000 16000 20000 )
+SLEEP_KNOBS=( 16000 3000 )
+#SLEEP_KNOBS=( 16000 )
+#FD_DATA_KNOBS=( 6250 62500 625000 6250000 )
+#FD_DATA_KNOBS=( 6250 )
+FD_DATA_KNOBS=( 0 )
+#SD_DATA_KNOBS=( 1 2 )
+SD_DATA_KNOBS=( 2 )
+NUM_GPUS_SET=( "1" "2" "3" "4")
+#NUM_GPUS_SET=("1")
+CUDA_VISIBLE_DEVICES_SET=( "0" "0,1" "0,1,2" "0,1,2,3" )
+#CUDA_VISIBLE_DEVICES_SET=( "0" )
+USER_CHOSEN_PLACEMENT_SET=( "0" "1" )
+#USER_CHOSEN_PLACEMENT_SET=( "0" )
+GIL_COUNT=1
+GIL_TIME=0
+
+OUT_ITERS=( "1" "2" "3" )
+#OUT_ITERS=( "1" )
+
+#DATA_MOVE_MODES=( 0 1 2 )
+DATA_MOVE_MODES=( 0 )
+#DATA_MOVE_MODES=( 2 )
+
+GRAPH_DIR="graphs"
+
+GRAPH_INPUT_DIR="asplos24_nodata_input"
+rm -rf $GRAPH_INPUT_DIR
+mkdir $GRAPH_INPUT_DIR
+
+OUTPUT_DIR="asplos24_nodata_output"
+rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
 
 SOURCE=${BASH_SOURCE[0]}
 DIR="$( dirname "${SOURCE}" )"
 
-PLACEMENT_MODE=( "fixed-placement" "policy" )
-
-#DATA_MOVE_MODE=( "no" "lazy" "eager" )
-DATA_MOVE_MODE=( "eager" )
-
-
-NUM_GPUS=4
-for d_mode in "${DATA_MOVE_MODE[@]}"; do
-  for ps in "${PLACEMENT_MODE[@]}"; do
-    for ((i=0; i<NUM_GPUS; ++i)); do
-      CUDA_VISIBLE_DEVICES=""
-      for ((j=0; j<=i; ++j)); do
-          CUDA_VISIBLE_DEVICES+=$j
-          if [ $j -lt $i ]
-          then
-              CUDA_VISIBLE_DEVICES+=","
-          fi
+for out_iter in "${OUT_ITERS[@]}"; do
+  for GRAPH_TYPE in "${GRAPH_TYPES_STR[@]}"; do
+    for computation_time in "${SLEEP_KNOBS[@]}"; do
+      for fd_data_knob in "${FD_DATA_KNOBS[@]}"; do
+        for sd_data_knob in "${SD_DATA_KNOBS[@]}"; do 
+          for user_chosen_placement in "${USER_CHOSEN_PLACEMENT_SET[@]}"; do
+            if [[ ${GRAPH_TYPE} == *"reduction"* ]]; then
+              for level in "${LEVELS[@]}"; do
+                for num_gpus in "${!NUM_GPUS_SET[@]}"; do
+                  ng=$((num_gpus + 1))
+                  for data_move_mode in "${DATA_MOVE_MODES[@]}"; do
+                    FLAGS=" -computation_weight "${computation_time}" -gil_count "$GIL_COUNT" -gil_time "$GIL_TIME" -user "$user_chosen_placement" -num_gpus "$ng
+                    FLAGS+=" -overlap 1 -level "${level}" -branch 2 -d "${sd_data_knob}" -data_move "${data_move_mode}
+                    FLAGS+=" -workloads reduction -width_bytes "${fd_data_knob}" -iter "${out_iter}
+                    output_prefix="${GRAPH_TYPE}_${fd_data_knob}_${sd_data_knob}_${computation_time}_${user_chosen_placement}_${level}_${ng}_${data_move_mode}_${out_iter}"
+                    if [[ ${fd_data_knob} == 0 ]]; then
+                        output_prefix+=".nodata"
+                    fi
+                    output_fname=${output_prefix}.log
+                    commands="python "${DIR}"/benchmark.py -graph ${GRAPH_INPUT_DIR}/${output_prefix}.gph "$FLAGS
+                    echo $commands
+                    CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES_SET[$num_gpus]} $commands > $OUTPUT_DIR/${output_fname}
+                    grep "reduction," ${OUTPUT_DIR}/${output_fname} >> ${OUTPUT_DIR}/result.out
+                  done
+                done
+              done
+            else
+              for num_task in "${NUM_TASKS_SET[@]}"; do
+                for num_gpus in "${!NUM_GPUS_SET[@]}"; do
+                  ng=$((num_gpus + 1))
+                  for data_move_mode in "${DATA_MOVE_MODES[@]}"; do
+                    FLAGS=" -computation_weight "${computation_time}" -gil_count "$GIL_COUNT" -gil_time "$GIL_TIME" -user "$user_chosen_placement" -num_gpus "$ng
+                    FLAGS+=" -d "${sd_data_knob}" -data_move "${data_move_mode}
+                    FLAGS+=" -width_bytes "${fd_data_knob}" -iter "${out_iter}
+                    if [[ ${GRAPH_TYPE} == *"independent"* ]]; then
+                      FLAGS+=" -overlap 0 -n "${num_task}" -workloads independent"
+                    elif [[ ${GRAPH_TYPE} == *"serial"* ]]; then
+                      FLAGS+=" -overlap 1 -n "${num_task}" -workloads serial"
+                    fi
+                    output_prefix="${GRAPH_TYPE}_${fd_data_knob}_${sd_data_knob}_${computation_time}_${user_chosen_placement}_${num_task}_${ng}_${data_move_mode}_${out_iter}"
+                    if [[ ${fd_data_knob} == 0 ]]; then
+                        output_prefix+=".nodata"
+                    fi
+                    commands="python "${DIR}"/benchmark.py -graph ${GRAPH_INPUT_DIR}/${output_prefix}.gph "$FLAGS
+                    output_fname=${output_prefix}.log
+                    echo $output_prefix
+                    echo $commands
+                    CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES_SET[$num_gpus]} $commands > $OUTPUT_DIR/${output_fname}
+                    if [[ ${GRAPH_TYPE} == *"independent"* ]]; then
+                        grep "independent," ${OUTPUT_DIR}/${output_fname} >> ${OUTPUT_DIR}/result.out
+                    elif [[ ${GRAPH_TYPE} == *"serial"* ]]; then
+                        grep "serial," ${OUTPUT_DIR}/${output_fname} >> ${OUTPUT_DIR}/result.out
+                    fi
+                  done
+                done
+              done
+            fi
+          done
+        done
       done
-      CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python ${DIR}/benchmark.py $((i+1)) $ps $d_mode
-      echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python ${DIR}/benchmark.py $((i+1)) $ps $d_mode"
     done
   done
 done
