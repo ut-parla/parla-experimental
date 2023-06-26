46cc196387466a2d25d387aee43033ea4f593c98
Hochan Lee <hochan@utexas.edu>
2023-06-26 11:26:48 -0500

    Refine mm python comment

--- src/python/parla/cython/mm.pyx
+++ src/python/parla/cython/mm.pyx
@@ -7,16 +7,13 @@ from parla.cython cimport device_manager
 
 class PyMM:
     def __init__(self, dm: device_manager.PyDeviceManager):
-        #print("PyMM constructor", flush=True)
         self._device_manager = device_manager
         self._cy_mm = CyMM(dm.get_cy_device_manager())
-        #print("PyMM constructor [done]", flush=True)
 
     def size(self, dev_id: int):
         return self._cy_mm.size(dev_id)
 
     def remove_and_return_head_from_zrlist(self, dev_id: int):
-        print("request head of zrlist", flush=True)
         return self._cy_mm.remove_and_return_head_from_zrlist(dev_id)
 
     def get_cy_memory_manager(self):
@@ -35,19 +32,15 @@ class PyMM:
                   f"\t GPU{device_id} {label} Free bytes: {mempool.free_bytes()} \n"
                   f"\t GPU{device_id} {label} Total bytes: {mempool.total_bytes()} \n"), flush=True)
         except ImportError:
-            print("MM tracker only tracks CuPy memory for now.", flush=True)
+            print("MM tracker only supports CuPy memory status checking.", flush=True)
 
 
 cdef class CyMM:
-#cdef LRUGlobalEvictionManager* _inner_mm
 
     def __cinit__(self, device_manager.CyDeviceManager cy_dm):
-        print("CyMM cinit()", flush=True)
         self._inner_mm = new LRUGlobalEvictionManager(cy_dm.get_cpp_device_manager())
-        print("CyMM cinit() [done]", flush=True)
 
     def __dealloc__(self):
-        print("Deallocation CyMM\n")
         del self._inner_mm
 
     cpdef size(self, int dev_id):
@@ -56,10 +49,11 @@ cdef class CyMM:
 
     cpdef remove_and_return_head_from_zrlist(self, int dev_id):
         cdef LRUGlobalEvictionManager* c_self = self._inner_mm
-        print("cpp request head of zrlist", flush=True)
         cdef void* py_parray = c_self.remove_and_return_head_from_zrlist(dev_id)
-        print("cpp requested head ", flush=True)
         if py_parray == NULL:
+            # TODO(hc): This path is actually not used.
+            # It would be great if we can check if this python object is valid
+            # at here; it can simplify our current mechanism a lot.
             return None
         else:
             return <object> py_parray
