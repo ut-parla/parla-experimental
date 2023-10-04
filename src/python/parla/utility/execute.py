21a80e87032dc9843d01f9de69865093f19acdba
dialecticDolt <will.ruys@gmail.com>
2023-10-03 20:18:04 -0500

    Change estimate_frequency to target time version

--- src/python/parla/utility/execute.py
+++ src/python/parla/utility/execute.py
@@ -30,6 +30,8 @@ from parla.common.parray.from_data import asarray
 from parla.cython.device_manager import cpu, gpu
 from parla.cython.variants import specialize
 from parla.cython.core import gpu_bsleep_nogil
+from typing import Tuple
+
 import numpy as np
 
 from fractions import Fraction
@@ -43,34 +45,84 @@ def make_parrays(data_list):
         parray_list.append(asarray(data, name="data"+str(i)) )
     return parray_list
 
-
-def estimate_frequency(n_samples=10, ticks=1900000000):
+def _get_time_for_cycles(sleep_func: Callable, cycles: int, samples=10) -> Tuple[float, float]:
     import cupy as cp
-    stream = cp.cuda.get_current_stream()
-    cycles = ticks
-    device_id = 0
+    import numpy as np
 
-    print(f"Starting GPU Frequency benchmark.")
-    times = np.zeros(n_samples)
-    for i in range(n_samples):
-
+    observed_times = []
+    for k in range(samples):
+        stream = cp.cuda.get_current_stream()
         start = time.perf_counter()
-        gpu_bsleep_nogil(device_id, int(ticks), stream)
+        sleep_func(0, cycles, stream)
         stream.synchronize()
         end = time.perf_counter()
-        print(f"...collected frequency sample {i} ", end-start)
+        elapsed = end - start
+        observed_times.append(elapsed)
 
-        times[i] = (end-start)
+    times = np.asarray(observed_times)
+    return np.mean(times), np.std(times)
 
-    times = times[2:]
-    elapsed = np.mean(times)
-    estimated_speed = cycles/np.mean(times)
-    median_speed = cycles/np.median(times)
 
-    print("Finished Benchmark.")
-    print("Estimated GPU Frequency: Mean: ", estimated_speed,
-          ", Median: ", median_speed, flush=True)
-    return estimated_speed
+def _get_time_for_cycles_event(sleep_func: Callable, cycles: int, samples=10) -> Tuple[float, float]:
+    import cupy as cp
+    import numpy as np
+    observed_times = []
+    for k in range(samples):
+        stream = cp.cuda.get_current_stream()
+        start_event = cp.cuda.Event()
+        end_event = cp.cuda.Event()
+
+        start_event.record(stream)
+        sleep_func(0, cycles, stream)
+        end_event.record(stream)
+        stream.synchronize()
+        event_elapsed = cp.cuda.get_elapsed_time(
+            start_event, end_event)/1000
+        observed_times.append(event_elapsed)
+
+    times = np.asarray(observed_times)
+    return np.mean(times), np.std(times)
+
+def estimate_frequency(sleep_func: Callable, samples=30, initial: int = 100000000, target_time=10000, verbose=False, use_event=True, tol=10) -> int:
+    import cupy as cp
+    import numpy as cp
+
+    target_time = target_time / (1000 * 1000)
+    tol = tol / (1000 * 1000)
+
+    ticks = initial * target_time
+
+    if verbose:
+        print("Starting GPU Frequency benchmark.")
+        print("Target Time: ", target_time, flush=True)
+
+    times = []
+
+    for k in range(samples):
+
+        if use_event:
+            mean_time, std_time = _get_time_for_cycles_event(
+                sleep_func, ticks, samples=k+10)
+        else:
+            mean_time, std_time = _get_time_for_cycles(
+                sleep_func, ticks, samples=k+10)
+
+        times.append(mean_time)
+
+        # Update Estimate
+        ticks = (ticks) * (target_time/times[-1])
+
+        if verbose:
+            print(f"Observed Time: {mean_time}, STD: {std_time}", flush=True)
+            print(f"Estimated Frequency: {ticks / times[-1]} Hz", flush=True)
+
+        if k > 0:
+            diff = np.abs(mean_time - target_time)
+            if diff < tol:
+                break
+
+    final_estimate = ticks
+    return int(final_estimate)
 
 
 class GPUInfo():
