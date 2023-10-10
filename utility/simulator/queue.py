9e28486b7e2d59304067cb8ed444386f98e866d3
dialecticDolt <will.ruys@gmail.com>
2023-10-09 22:00:36 -0500

    Extend QueueIterator

--- utility/simulator/queue.py
+++ utility/simulator/queue.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
+
 import heapq
 from ..types import TaskID, Time
 from .events import Event
 from typing import Tuple, TypeVar, Optional, Self
-from .task import SimulatedTask
 
 # (completion_time, event)
 EventPair = Tuple[Time, Event]
@@ -26,12 +27,17 @@ class PriorityQueue:
     def __len__(self):
         return len(self.queue)
 
+    def __str__(self):
+        return f"PriorityQueue({self.queue})"
 
-class QueueDrainer(object):
-    def __init__(self, q: PriorityQueue, maxiter: int = -1):
+
+class QueueIterator(object):
+    def __init__(self, q: PriorityQueue, maxiter: int = -1, peek: bool = False):
         self.q = q
         self.iter = 0
         self.maxiter = maxiter
+        self.peek = peek
+        self.success_count = 0
 
     def __iter__(self):
         return self
@@ -45,11 +51,29 @@ class QueueDrainer(object):
             raise StopIteration
 
         self.iter += 1
+
+        if self.peek:
+            return self.q.peek()
+        else:
+            self.success_count += 1
+            return self.q.get()
+
+    def pop(self):
+        self.success_count += 1
         return self.q.get()
+
+    def __len__(self):
+        return len(self.q)
+
+    def __str__(self):
+        return f"QueueIterator({self.q}, ITER={self.iter}, MAXITER={self.maxiter}, PEEK={self.peek})"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class TaskQueue(PriorityQueue):
-    def put(self, task: SimulatedTask):
+    def put(self, task: "SimulatedTask"):
         super().put((task.info.order, task.name))
 
     def get(self) -> Optional[TaskID]:
@@ -66,16 +90,25 @@ class TaskQueue(PriorityQueue):
         else:
             return None
 
+    def __str__(self):
+        return f"TaskQueue({self.queue})"
 
-class GetNextTask(QueueDrainer):
-    def __init__(self, q: TaskQueue, maxiter: int = -1):
-        super().__init__(q, maxiter)
+
+class TaskIterator(QueueIterator):
+    def __init__(self, q: TaskQueue, maxiter: int = -1, peek: bool = False):
+        super().__init__(q, maxiter, peek)
 
     def __iter__(self) -> Self:
         return super().__iter__()
 
     def __next__(self) -> TaskPair:
         return super().__next__()
+
+    def pop(self) -> TaskPair:
+        return super().pop()
+
+    def __str__(self):
+        return f"TaskIterator({self.q}, ITER={self.iter}, MAXITER={self.maxiter}, PEEK={self.peek})"
 
 
 class EventQueue(PriorityQueue):
@@ -92,13 +125,22 @@ class EventQueue(PriorityQueue):
         else:
             return None
 
+    def __str__(self):
+        return f"EventQueue({self.queue})"
 
-class GetNextEvent(QueueDrainer):
-    def __init__(self, q: EventQueue, maxiter: int = -1):
-        super().__init__(q, maxiter)
+
+class EventIterator(QueueIterator):
+    def __init__(self, q: EventQueue, maxiter: int = -1, peek: bool = False):
+        super().__init__(q, maxiter, peek)
 
     def __iter__(self) -> Self:
         return super().__iter__()
 
     def __next__(self) -> EventPair:
         return super().__next__()
+
+    def pop(self) -> EventPair:
+        return super().pop()
+
+    def __str__(self):
+        return f"EventIterator({self.q}, ITER={self.iter}, MAXITER={self.maxiter}, PEEK={self.peek})"
--- utility/simulator/schedulers/parla.py
+++ utility/simulator/schedulers/parla.py
@@ -14,12 +14,12 @@ from typing import List, Dict, Set, Tuple, Optional, Callable
 from dataclasses import dataclass, InitVar
 from collections import defaultdict as DefaultDict
 
-from .scheduler import SchedulerArchitecture, SystemState, register_scheduler
+from .scheduler import SchedulerArchitecture, SystemState, SchedulerOptions
 
 from rich import print
 
 
-@register_scheduler("parla")
+@SchedulerOptions.register_scheduler("parla")
 @dataclass(slots=True)
 class ParlaArchitecture(SchedulerArchitecture):
     topology: SimulatedTopology
@@ -55,92 +55,31 @@ class ParlaArchitecture(SchedulerArchitecture):
             self.launchable_tasks[device.name][TaskType.DATA] = TaskQueue()
             self.launchable_tasks[device.name][TaskType.COMPUTE] = TaskQueue()
 
-    def add_initial_task(self, task: SimulatedTask):
+    def initialize(self, tasks: List[SimulatedTask]) -> List[EventPair]:
+        # Initialize the set of visible tasks
+        self.add_initial_tasks(tasks)
+
+        # Initialize the event queue
+        next_event = Mapper()
+        next_time = Time(0)
+        return [(next_time, next_event)]
+
+    def add_initial_tasks(self, tasks: List[SimulatedTask]):
         """
         Append an initial task who does not have any dependency to
         a spawned task queue.
         """
-        self.spawned_tasks.put(task.name)
+        for task in tasks:
+            self.spawned_tasks.put(task)
 
     def launcher(
         self, scheduler_state: SystemState, event: Launcher
     ) -> List[EventPair]:
         return []
-        # i = 0
-        # max_tasks = event.max_tasks
-
-        # while i < max_tasks:
-
-        #     for device in self.devices:
-        #         reserved_tasks = self.reserved_tasks[device]
-        #         data_tasks = reserved_tasks[TaskType.DATA]
-        #         compute_tasks = reserved_tasks[TaskType.COMPUTE]
-
-        #         active_queue = data_tasks
-
-        #         # Check if there are any tasks that can be launched
-
-        #         task = active_queue.peek()
-        #         if task
-        #         if self.check_resources(task, self.resource_pool):
-        #             task = active_queue.get()
-        #             duration = estimate_time(task, self.resource_pool)
-        #             start_task(task)
-
-        #         if task:
-        #             print("Assigning Task:", task.name, task.dependencies)
-
-        #             # data0 = task.read_data[0]
-        #             # print(data0)
-
-        #             # Assign memory
-        #             task.reserve_resources(self.resource_pool)
-
-        #             # Compute time to completion
-        #             task.estimate_time(self.time, self.resource_pool)
-        #             # print("Expected Complete: ", task.completion_time)
-        #             # "Start" Task
-        #             # 1. Locks data (so it can't be evicted)
-        #             # 2. Updates data status (to evict stale data)
-        #             task.start()
-
-        #             # Lock used data
-        #             # task.lock_data()
-
-        #             # Push task to global active queue
-        #             self.active_tasks.put((task.completion_time, task))
-
-        #             # Push task to local active queue (for device)
-        #             # NOTE: This is mainly just as a convience for logging
-        #             device.push_local_active_task(task)
-
-        #             # data0 = task.read_data[0]
-        #             # print(data0)
-
-        #             # Update global state log
-        #             self.state.set_task_log(
-        #                 task.name, "status", "active")
-        #             self.state.set_task_log(
-        #                 task.name, "start_time", self.time)
-        #         else:
-        #             continue
 
     def complete_task(
         self, scheduler_state: SystemState, event: TaskCompleted
     ) -> List[EventPair]:
-        objects = scheduler_state.objects
-        assert objects is not None
-        task = objects.get_task(event.task)
-
-        # Stop reserving memory
-        # recent_task.free_resources(self.resource_pool)
-
-        # Remove from active device queues
-        # for device in recent_task.locations:
-        #       self.devicespace[device].pop_local_active_task()
-
-        # Update dependencies
-        # recent_task.finish()
         return []
 
     def map_task(self, scheduler_state: SystemState, event: Mapper) -> List[EventPair]:
