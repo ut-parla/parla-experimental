24abe7155d10237bd4d9d5f46019f6169f4f1736
dialecticDolt <will.ruys@gmail.com>
2023-10-20 11:30:10 -0500

    BUGFIX: Make crosspy optional

--- src/python/parla/common/dataflow.py
+++ src/python/parla/common/dataflow.py
@@ -2,10 +2,9 @@
 @file dataflow.py
 @brief Implements internal CrossPy containers for automatic datamovement.
 """
-from __future__ import annotations # For type hints of unloaded classes
+from __future__ import annotations  # For type hints of unloaded classes
 from parla.common.parray.core import PArray
 from parla.common.globals import CROSSPY_ENABLED, crosspy
-from crosspy import CrossPyArray
 
 from typing import List, Tuple, Union
 
@@ -24,19 +23,20 @@ class DataflowIterator:
         Return the next value from Dataflow's data lists:
         input -> output -> in/output lists
         """
-        if self._idx < (len(self._df._input) + len(self._df._output) +
-                        len(self._df._inout)):
+        if self._idx < (
+            len(self._df._input) + len(self._df._output) + len(self._df._inout)
+        ):
             if self._idx < len(self._df._input):
                 # First, iterate input data operands.
                 cur_item = self._df._input[self._idx][0]
             elif self._idx < (len(self._df._input) + len(self._df._output)):
                 # Second, iterate output data operands.
-                cur_item = self._df._output[self._idx -
-                                            len(self._df._input)][0]
+                cur_item = self._df._output[self._idx - len(self._df._input)][0]
             else:
                 # Third, iterate input/output data operands.
-                cur_item = self._df._inout[self._idx - len(self._df._input) -
-                                           len(self._df._output)][0]
+                cur_item = self._df._inout[
+                    self._idx - len(self._df._input) - len(self._df._output)
+                ][0]
             self._idx += 1
             return cur_item
         raise StopIteration
@@ -49,9 +49,12 @@ class Dataflow:
     The dataflow can be iterated through DataflowIterator.
     """
 
-    def __init__(self, input: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]],
-                 output: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]],
-                 inout: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]]):
+    def __init__(
+        self,
+        input: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]],
+        output: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]],
+        inout: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]],
+    ):
         self._input = self.process_crosspys(input)
         self._output = self.process_crosspys(output)
         self._inout = self.process_crosspys(inout)
@@ -75,8 +78,8 @@ class Dataflow:
         return self._inout
 
     def process_crosspys(
-        self, _in: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]]) \
-            -> List[Tuple[PArray, int]]:
+        self, _in: List[Union[crosspy.CrossPyArray, Tuple[PArray, int]]]
+    ) -> List[Tuple[PArray, int]]:
         """
         Check elements of IN/OUT/INOUT parameters in @spawn
         and convert to Tuple[PArray, int] if an element is a CrossPyArray
@@ -99,8 +102,10 @@ class Dataflow:
                                 # Skip CuPy or NumPy arrays.
                                 _out.append((parray, i))
                 else:
-                    raise TypeError("IN/OUT/INOUT should be either a tuple of PArray",
-                                    "and its partition number or CrossPyArray")
+                    raise TypeError(
+                        "IN/OUT/INOUT should be either a tuple of PArray",
+                        "and its partition number or CrossPyArray",
+                    )
         return _out
 
     def __iter__(self):
