9fb67f687c4ec778d8ad54a8d5997534d5f272c8
wlruys <will.ruys@gmail.com>
2024-01-10 16:14:40 -0600

    add: print logging

--- src/python/parla/common/parray/core.py
+++ src/python/parla/common/parray/core.py
@@ -217,6 +217,9 @@ class PArray:
 
     # Public API:
 
+    def set_name(self, name: str):
+        self._name = name
+
     def get(self, device: Optional[PyDevice] = None) -> 'np.ndarray' | 'cp.ndarray':
         if device is None:
             return self.array
